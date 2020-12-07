ff294c16b2bae4512260bcf05ccb3445cf6849e8
vladimirfomene <vladimirfomene@gmail.com>
2020-12-07 09:14:14 +0100

    implement agent authentication

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <Page>
+        <ActionBar title="Welcome to Loan App!"/>
+        <FlexboxLayout justifyContent="center" alignItems="center" marginTop="30%" backgroundColor="#3c495e">
+            <TextField v-model="phoneNumber" />
+            <TextField v-model="password" />
+            <Button text="Login" @tap="login" />
+        </FlexboxLayout>
+    </Page>
+</template>
+
+
+<script>
+import { PHONE_NUMBER, PASSWORD } from "../utils/utils";
+import Customers from "./Customers.vue";
+
+export default {
+    name: "Login",
+    data(){
+        return {
+            phoneNumber: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            if(PHONE_NUMBER === this.phoneNumber && PASSWORD === this.password){
+                this.$navigateTo(Customers);
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import "../../node_modules/bulma/css/bulma.css";
+</style>
