d1afe4b76260d4325a1a62f751bcbb77db1c527f
vladimirfomene <vladimirfomene@gmail.com>
2020-12-07 09:13:58 +0100

    create welcome page which embeds login page

---
+++ src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <Page>
+        <Login />
+    </Page>
+</template>
+
+<script >
+import Login from "./Login.vue";
+
+  export default {
+    data() {
+      return {
+        msg: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+@import "../../node_modules/bulma/css/bulma.css";
+</style>
