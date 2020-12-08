e06811c89817ae677cd74dd4294277ec0e75b75f
vladimirfomene <vladimirfomene@gmail.com>
2020-12-08 17:28:21 +0100

    turn app page into login page

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,21 +1,39 @@
 <template>
     <Page>
-        <Login />
+        <ActionBar title="Welcome to Loan App!"/>
+        <FlexboxLayout justifyContent="center" flexDirection="column" alignItems="center" marginTop="5%">
+            <TextField v-model="phoneNumber" backgroundColor="#f9f9f9" maxLength="17" hint="Enter Your Phonenumber" marginBottom="2%"/>
+            <TextField v-model="password" backgroundColor="#f9f9f9" maxLength="17" hint="Enter Your Password" marginBottom="2%"/>
+            <Button text="Login" @tap="login" />
+        </FlexboxLayout>
     </Page>
 </template>
 
 <script >
-import Login from "./Login.vue";
+import util from "../utils/utils";
+import Customers from "./Customers";
 
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
+export default {
+    name: "Login",
+    components: {
+      Customers
+    },
+    data(){
+        return {
+            phoneNumber: "",
+            password: ""
+        }
+    },
+    methods: {
+        login(){
+            //util.PHONE_NUMBER === String(this.phoneNumber).trim() && util.PASSWORD === String(this.password).trim()
+            if(true){
+                this.$navigateTo(Customers);
+            }
+        }
     }
-  }
+}
 </script>
 
 <style scoped>
-@import "../../node_modules/bulma/css/bulma.css";
 </style>
